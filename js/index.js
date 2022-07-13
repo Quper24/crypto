954b0e8ac30bd0281743ef3673d0a6d9bdbeddd5
maksim.leskin <maksim.leskin@methed.ru>
2022-07-13 15:15:23 +0300

    sync lesson

--- js/index.js
+++ js/index.js
@@ -3,4 +3,4 @@ const headerNavigation = document.querySelector('.header__navigation');
 
 headerNavBtn.addEventListener('click', () => {
   headerNavigation.classList.toggle('header__navigation_open');
-});
+})
